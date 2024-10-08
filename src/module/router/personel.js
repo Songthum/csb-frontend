b429adbe9b091aeaf1bb52049fd8ae3fe30eac8b
Yume <92096632+Natcharika@users.noreply.github.com>
2024-10-08 14:37:18 +0700

    routerT

--- src/module/router/personel.js
+++ src/module/router/personel.js
@@ -5,6 +5,12 @@ import InputScoreCSB02 from '../page/teacher/input-score/inputscore-csb02';
 import InputScoreCSB04 from '../page/teacher/input-score/inputscore-csb04';
 import DataTable from '../page/teacher/request-lecture';
 import ChairmanScoreCSB01 from '../page/teacher/chairman-score/chairman-score-csb01';
+import ChairmanScoreCSB02 from '../page/teacher/chairman-score/chairman-score-csb02';
+import ChairmanScoreCSB04 from '../page/teacher/chairman-score/chairman-score-csb04';
+import DepartmentHeadScoreCSB01 from '../page/teacher/department-score/department-score-csb01';
+import DepartmentHeadScoreCSB02 from '../page/teacher/department-score/department-score-csb02';
+import DepartmentHeadScoreCSB04 from '../page/teacher/department-score/department-score-csb04';
+import ProjectApproval from '../page/teacher/approve-csb03';
 
 export default function Teacher(){
 
@@ -73,12 +79,84 @@ export default function Teacher(){
           element: (
             <SideBar 
               page={ <ChairmanScoreCSB01/>}
-              pageName={"อนุมัติโดยประธานกรรมการสอบ"}
+              pageName={"อนุมัติคะแนนสอบหัวข้อโดยประธานกรรมการสอบ"}
               pageSub={""}
               path={"/chairman-score/chairman-score-csb01"}
               rolePage={"personel"}
             />
           ),
-        }
+        },
+        {
+          path: "/chairman-score/chairman-score-csb02",
+          element: (
+            <SideBar 
+              page={ <ChairmanScoreCSB02/>}
+              pageName={"อนุมัติคะแนนสอบก้าวหน้าโดยประธานกรรมการสอบ"}
+              pageSub={""}
+              path={"/chairman-score/chairman-score-csb02"}
+              rolePage={"personel"}
+            />
+          ),
+        },
+        {
+          path: "/chairman-score/chairman-score-csb04",
+          element: (
+            <SideBar 
+              page={ <ChairmanScoreCSB04/>}
+              pageName={"อนุมัติคะแนนสอบป้องกันโดยประธานกรรมการสอบ"}
+              pageSub={""}
+              path={"/chairman-score/chairman-score-csb04"}
+              rolePage={"personel"}
+            />
+          ),
+        },
+        {
+          path: "/department-score/department-score-csb01",
+          element: (
+            <SideBar 
+              page={ <DepartmentHeadScoreCSB01/>}
+              pageName={"อนุมัติคะแนนสอบหัวข้อโดยหัวหน้าภาควิชา"}
+              pageSub={""}
+              path={"/department-score/department-score-csb01"}
+              rolePage={"personel"}
+            />
+          ),
+        },
+        {
+          path: "/department-score/department-score-csb02",
+          element: (
+            <SideBar 
+              page={ <DepartmentHeadScoreCSB02/>}
+              pageName={"อนุมัติคะแนนสอบก้าวหน้าโดยหัวหน้าภาควิชา"}
+              pageSub={""}
+              path={"/department-score/department-score-csb02"}
+              rolePage={"personel"}
+            />
+          ),
+        },
+        {
+          path: "/department-score/department-score-csb04",
+          element: (
+            <SideBar 
+              page={ <DepartmentHeadScoreCSB04/>}
+              pageName={"อนุมัติคะแนนสอบก้าวหน้าโดยหัวหน้าภาควิชา"}
+              pageSub={""}
+              path={"/department-score/department-score-csb04"}
+              rolePage={"personel"}
+            />
+          ),
+        },
+        {
+          path: "/approve-csb03",
+          element: (
+            <SideBar 
+              page={ <ProjectApproval/>}
+              pageName={"อนุมัติการยื่นทดสอบโครงงาน"}
+              pageSub={""}
+              path={"/approve-csb03"}
+              rolePage={"personel"}
+            />
+          ),
+        },
     ]
 };
